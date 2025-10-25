3fecacbbfbe5fa7777dab03ab320e69a769d31af
Sebastian Tatara <seba.kontakt@wp.pl>
2025-10-25 21:17:20 +0200

    Stworzenie pliku Node.h

--- Node.h
+++ Node.h
@@ -3,15 +3,21 @@
 
 /**
  * @file Node.h
- * @brief Węzeł listy dwukierunkowej
+ * @brief Definicja wezla listy dwukierunkowej
+ *
+ * Kazdy wezel przechowuje wartosc typu T oraz wskazniki na poprzedni i nastepny element listy.
  */
 
 template<typename T>
 struct Node {
-    T data;
-    Node<T>* prev;
-    Node<T>* next;
+    T data;          ///< Wartosc przechowywana w wezle
+    Node<T>* prev;   ///< Wskaznik na poprzedni element
+    Node<T>* next;   ///< Wskaznik na nastepny element
 
+    /**
+     * @brief Konstruktor wezla
+     * @param value Wartosc przechowywana w wezle
+     */
     Node(const T& value) : data(value), prev(nullptr), next(nullptr) {}
 };
 
