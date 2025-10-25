2819313e8853d9188b53316ce402dafd49c44484
Sebastian Tatara <seba.kontakt@wp.pl>
2025-10-25 21:16:10 +0200

    Stworzenie pliku IListIterator.h

--- IListIterator.h
+++ IListIterator.h
@@ -3,17 +3,31 @@
 
 /**
  * @file IListIterator.h
- * @brief Interfejs iteratora
+ * @brief Interfejs iteratora dla listy
+ *
+ * Definiuje podstawowe operacje iteratora: poruszanie się po liście
+ * oraz pobieranie wartości bieżącego elementu.
  */
 
 template<typename T>
 class IListIterator {
 public:
+    /** Wirtualny destruktor */
     virtual ~IListIterator() = default;
+
+    /** Sprawdza, czy istnieje następny element */
     virtual bool has_next() const = 0;
+
+    /** Sprawdza, czy istnieje poprzedni element */
     virtual bool has_prev() const = 0;
+
+    /** Przesuwa iterator na następny element */
     virtual void next() = 0;
+
+    /** Przesuwa iterator na poprzedni element */
     virtual void prev() = 0;
+
+    /** Zwraca referencję do wartości bieżącego elementu */
     virtual T& value() = 0;
 };
 
