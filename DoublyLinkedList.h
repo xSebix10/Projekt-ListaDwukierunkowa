2cee42c3c4e6c457f5e24949cf37ec7eeb3c0231
Sebastian Tatara <seba.kontakt@wp.pl>
2025-10-25 21:11:15 +0200

    Stworzenie pliku DoublyLinkedList.h

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -9,37 +9,57 @@
 
 /**
  * @file DoublyLinkedList.h
- * @brief Implementacja listy dwukierunkowej na stercie
+ * @brief Lista dwukierunkowa przechowywana na stercie
  */
 
 template<typename T>
 class DoublyLinkedList : public IList<T> {
 private:
-    Node<T>* head;
-    Node<T>* tail;
-    size_t size_;
+    Node<T>* head; ///< Wskaznik na pierwszy element
+    Node<T>* tail; ///< Wskaznik na ostatni element
+    size_t size_;  ///< Liczba elementow
+
 public:
+    /** Konstruktor - tworzy pusta liste */
     DoublyLinkedList();
+
+    /** Destruktor - usuwa wszystkie elementy */
     ~DoublyLinkedList();
 
-    // podstawowe operacje
-    void push_front(const T &value) override;
-    void push_back(const T &value) override;
-    void insert_at(size_t index, const T &value) override;
+    /** Dodaje element na poczatek listy */
+    void push_front(const T& value) override;
 
+    /** Dodaje element na koniec listy */
+    void push_back(const T& value) override;
+
+    /** Dodaje element na wskazany indeks */
+    void insert_at(size_t index, const T& value) override;
+
+    /** Usuwa element z poczatku listy */
     void pop_front() override;
+
+    /** Usuwa element z konca listy */
     void pop_back() override;
+
+    /** Usuwa element z pod wskazanego indeksu */
     void remove_at(size_t index) override;
 
+    /** Czysci cala liste */
     void clear() override;
 
+    /** Wyswietla liste od poczatku */
     void print_forward() const override;
+
+    /** Wyswietla liste od konca */
     void print_backward() const override;
 
+    /** Zwraca liczbe elementow */
     size_t size() const { return size_; }
 
-    // iterator helper
+    /** Zwraca iterator na pierwszy element */
     ListIterator<T> begin_iterator() const { return ListIterator<T>(head); }
+
+    /** Zwraca iterator na ostatni element */
     ListIterator<T> end_iterator() const { return ListIterator<T>(tail); }
 };
 
